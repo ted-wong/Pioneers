a64bb11b3787857b3c5d1e625b3469ecaffc6157
ted-wong <wong.ted.t@gmail.com>
2016-02-16 15:57:07 -0500

    First commmit

--- src/common.ts
+++ src/common.ts
@@ -73,6 +73,164 @@ interface Player {
   knightsPlayed: number;
   longestRoad: number;
   constructions: Constructions;
+
+  function canAffordConstruction(construct: number): boolean {
+    switch (construct) {
+      case Constructions.Road:
+        if (this.resources[0] >= 1 && this.resources[1] >= 1)
+          return true;
+        break;
+      case Constructions.Settlement:
+        if (this.resources[0] >= 1 && this.resources[1] >= 1 && this.resources[2] >= 1 && this.resources[3] >= 1)
+          return true;
+        break;
+      case Constructions.City:
+        if (this.resources[3] >= 2 && this.resources[4] >= 3)
+          return true;
+        break;
+      case Constructions.DevelopmentCard:
+        if (this.resources[2] >= 1 && this.resources[3] >= 1 && this.resources[4] >= 1)
+          return true;
+        break;
+      default:
+        return false;
+        break;
+    }
+    return false;
+  }
+  
+  function hasSuffucientConstructsToBuild(construct: number): boolean {
+    switch (construct) {
+      case Constructions.Road:
+        if (this.constructions[Constructions.Road] < 15)
+          return true;
+        break;
+      case Constructions.Settlement:
+        if (this.constructions[Constructions.Settlement] < 5)
+          return true;
+        break;
+      case Constructions.City:
+        if (this.constructions[Constructions.City] < 4 && this.constructions[Constructions.Settlement] > 0)
+          return true;
+        break;
+      case Constructions.DevelopmentCard:
+        return true;
+        break;
+      default:
+        return false;
+        break;
+    }
+    return false;
+  }
+  
+  function canBuildRoadLegally(board: Board, row: number, col: number, edge: number): boolean {
+  
+    if (edge < 0 || edge > 5) return false;
+    if (row < 0 || row > ROWS || col < 0 || col > COLS) return false;
+    
+    // edge must be empty - no other roads
+    if (board[row][col].edges[edge] >= 0) return false;
+    
+    let adjHex: number[] = getHexAdjcentToEdge(row, col, edge);
+    
+    // both hexes cannot be water
+    if (board[row][col].label = Resource.Water) {
+      if (board[adjHex[0]][adjHex[1]].label = Resource.Water) {
+        return false;
+      }
+    }
+    
+    // player owns adjacent road in current hex or adjacent road in adjacent hex
+    if (board[row][col].edges[((edge+1) % 6 + 6) % 6] = this.id || \
+      board[row][col].edges[((edge-1) % 6 + 6) % 6] = this.id || \
+      board[adjHex[0]][adjHex[1]].edges[((edge+3+1) % 6 + 6) % 6] = this.id || \
+      board[adjHex[0]][adjHex[1]].edges[((edge+3-1) % 6 + 6) % 6] = this.id) {
+      
+      // check if other player's settlement/city is inbetween existing road and proposed road
+      // cannot build through player's settlement/city, even with connecting road
+      
+      // building CC on same hex
+      if (board[row][col].edges[((edge+1) % 6 + 6) % 6] = this.id && \
+        ((board[row][col].vertices[edge] == Constructions.Settlement || \
+        board[row][col].vertices[edge] == Constructions.City) && \
+        board[row][col].vertexOwner[edge] != this.id)) {
+        
+        return false;
+      }
+      // building CW on same hex
+      if (board[row][col].edges[((edge-1) % 6 + 6) % 6] = this.id && \
+        ((board[row][col].vertices[((edge-1) % 6 + 6) % 6] == Constructions.Settlement || \
+        board[row][col].vertices[((edge-1) % 6 + 6) % 6] == Constructions.City) && \
+        board[row][col].vertexOwner[((edge-1) % 6 + 6) % 6] != this.id)) {
+        
+        return false;
+      }
+      // building CC on adj. hex
+      if (board[adjHex[0]][adjHex[1]].edges[((edge+3-1) % 6 + 6) % 6] = this.id && \
+        ((board[row][col].vertices[edge] == Constructions.Settlement || \
+        board[row][col].vertices[edge] == Constructions.City) && \
+        board[row][col].vertexOwner[edge] != this.id)) {
+        
+        return false;
+      }
+      // building CW on adj. hex
+      if (board[adjHex[0]][adjHex[1]].edges[((edge+3+1) % 6 + 6) % 6] = this.id && \
+        ((board[row][col].vertices[((edge-1) % 6 + 6) % 6] == Constructions.Settlement || \
+        board[row][col].vertices[((edge-1) % 6 + 6) % 6] == Constructions.City) && \
+        board[row][col].vertexOwner[((edge-1) % 6 + 6) % 6] != this.id)) {
+        
+        return false;
+      }
+      
+      return true;
+    }
+    return false;
+  }
+  
+  function canBuildSettlementLegally(board: Board, row: number, col: number, vertex: number): boolean {
+  
+    if (edge < 0 || edge > 5) return false;
+    if (row < 0 || row > ROWS || col < 0 || col > COLS) return false;
+    
+    // proposed vertex must be empty - no other settlement/city
+    if (board[row][col].vertices[vertex] != -1) return false;
+  
+    // needs adjacent road to build on
+    if (!hasAdjacentRoad(board, row, col, vertex)) return false;
+    
+    // new settlement has to be 2+ vertices away from another settlement/city
+    if (hasNearbyConstruct(board, row, col, vertex)) return false;
+    
+    return true;
+  }
+  
+  function canUpgradeSettlement(board: Board, row: number, col: number, vertex: number): boolean {
+  
+    if (edge < 0 || edge > 5) return false;
+    if (row < 0 || row > ROWS || col < 0 || col > COLS) return false;
+    
+    // proposed vertex must be empty - no other settlement/city
+    if (board[row][col].vertices[vertex] == Constructions.Settlement && \
+      board[row][col].VertexOwner[vertex] == this.id)
+      return true;
+    return false;
+  }
+  
+  function hasAdjacentRoad(board: Board, row: number, col: number, vertex: number): boolean {
+  
+    if (board[row][col].edges[vertex] = this.id) return true;
+    if (board[row][col].edges[(vertex+1) % 6] = this.id) return true;
+    
+    let [hex1, hex2] = getHexesAdjacentToVertex(row, col, vertex);
+    if (board[hex1[0]][hex1[1]].edges[hex1[3]] == this.id) return true;
+    if (board[hex1[0]][hex1[1]].edges[(hex1[3]+1) % 6] == this.id) return true;
+    
+    if (board[hex2[0]][hex2[1]].edges[hex2[3]] == this.id) return true;
+    if (board[hex2[0]][hex2[1]].edges[(hex2[3]+1) % 6] == this.id) return true;
+    
+    return false;
+  }
+
 }
 
 interface Bank {
@@ -112,3 +270,115 @@ interface StateDelta {
 interface IState extends StateDelta {
   delta: StateDelta;
 }
+
+// *****************
+// Helper functions for player interface
+// *****************
+
+
+
+function hasNearbyConstruct(board: Board, row: number, col: number, vertex: number): boolean {
+
+  if (board[row][col].vertices[vertex] == Construction.Settlement || \
+    board[row][col].vertices[vertex] == Construction.City)
+    return true;
+
+  if (board[row][col].vertices[(vertex+1) % 6] == Construction.Settlement || \
+    board[row][col].vertices[(vertex+1) % 6] == Construction.City || \
+    board[row][col].vertices[((vertex-1) % 6 + 6) % 6] == Construction.Settlement || \
+    board[row][col].vertices[((vertex-1) % 6 + 6) % 6] == Construction.City)
+    return true;
+  
+  let [hex1, hex2] = getHexesAdjacentToVertex(row, col, vertex);
+  
+  if (board[hex1[0]][hex1[1]].vertex[(hex1[3]+1) % 6] == Construction.Settlement || \
+    board[hex1[0]][hex1[1]].vertex[(hex1[3]+1) % 6] == Construction.City || \
+    board[hex1[0]][hex1[1]].vertices[((hex1[3]-1) % 6 + 6) % 6] == Construction.Settlement || \
+    board[hex1[0]][hex1[1]].vertices[((hex1[3]-1) % 6 + 6) % 6] == Construction.City)
+    return true;
+    
+  if (board[hex2[0]][hex2[1]].vertex[(hex2[3]+1) % 6] == Construction.Settlement || \
+    board[hex2[0]][hex2[1]].vertex[(hex2[3]+1) % 6] == Construction.City || \
+    board[hex2[0]][hex2[1]].vertices[((hex2[3]-1) % 6 + 6) % 6] == Construction.Settlement || \
+    board[hex2[0]][hex2[1]].vertices[((hex2[3]-1) % 6 + 6) % 6] == Construction.City)
+    return true;
+    
+  return false;
+}
+
+
+// return [row, col] pair that shares edge with original edge
+function getHexAdjcentToEdge(row: number, col: number, edge: number): number[] {
+
+  // TODO: add check for new row/col out of bounds
+
+  if (edge == 0)
+    return [row, col+1];
+
+  if (edge == 1 && row%2 == 0)
+    return [row-1, col+1];
+  if (edge == 1 && row%2 == 1)
+    return [row-1, col];
+
+  if (edge == 2 && row%2 == 0)
+    return [row-1, col];
+  if (edge == 2 && row%2 == 1)
+    return [row-1, col-1];
+
+  if (edge == 3)
+    return [row, col-1];
+
+  if (edge == 4 && row%2 == 0)
+    return [row+1, col];
+  if (edge == 4 && row%2 == 1)
+    return [row+1, col-1];
+
+  if (edge == 5 && row%2 == 0)
+    return [row+1, col+1];
+  if (edge == 5 && row%2 == 1)
+    return [row+1, col];
+
+  // default - shouldn't happen though
+  return [-1, -1];
+}
+
+// will return 2 hexes in [row, col, vertex] that share the original vertex
+function getHexesAdjacentToVertex(row: number, col: number, vertex: number): number[][] {
+
+  // TODO: add check for new row/col hexes out of bounds
+
+  if (vertex == 0 && row%2 == 0)
+    return [[row, col+1, 2], [row-1, col+1, 4]];
+  if (vertex == 0 && row%2 == 1)
+    return [[row, col+1, 2], [row-1, col, 4]];
+
+  if (vertex == 1 && row%2 == 0)
+    return [[row-1, col, 5], [row-1, col+1, 3]];
+  if (vertex == 1 && row%2 == 1)
+    return [[row-1, col-1, 5], [row-1, col, 3]];
+    
+  if (vertex == 2 && row%2 == 0)
+    return [[row, col-1, 0], [row-1, col, 4]];
+  if (vertex == 2 && row%2 == 1)
+    return [[row, col-1, 0], [row-1, col-1, 4]];
+
+  if (vertex == 3 && row%2 == 0)
+    return [[row, col-1, 5], [row+1, col, 1]];
+  if (vertex == 3 && row%2 == 1)
+    return [[row, col-1, 5], [row+1, col-1, 1]];
+
+  if (vertex == 4 && row%2 == 0)
+    return [[row+1, col, 0], [row+1, col+1, 2]];
+  if (vertex == 4 && row%2 == 1)
+    return [[row+1, col-1, 0], [row+1, col, 2]];
+
+  if (vertex == 5 && row%2 == 0)
+    return [[row+1, col+1, 1], [row, col+1, 3]];
+  if (vertex == 5 && row%2 == 1)
+    return [[row+1, col, 1], [row, col+1, 3]];
+
+  // default - shouldn't happen though
+  return [[-1, -1, -1], [-1, -1, -1]];
+}
+
+
--- ts_output_readonly_do_NOT_change_manually/src/common.js
+++ ts_output_readonly_do_NOT_change_manually/src/common.js
@@ -43,4 +43,248 @@ var MoveType;
     MoveType[MoveType["WIN"] = 13] = "WIN";
     MoveType[MoveType["SIZE"] = 14] = "SIZE";
 })(MoveType || (MoveType = {}));
+function canAffordConstruction(construct) {
+    switch (construct) {
+        case Constructions.Road:
+            if (this.resources[0] >= 1 && this.resources[1] >= 1)
+                return true;
+            break;
+        case Constructions.Settlement:
+            if (this.resources[0] >= 1 && this.resources[1] >= 1 && this.resources[2] >= 1 && this.resources[3] >= 1)
+                return true;
+            break;
+        case Constructions.City:
+            if (this.resources[3] >= 2 && this.resources[4] >= 3)
+                return true;
+            break;
+        case Constructions.DevelopmentCard:
+            if (this.resources[2] >= 1 && this.resources[3] >= 1 && this.resources[4] >= 1)
+                return true;
+            break;
+        default:
+            return false;
+            break;
+    }
+    return false;
+}
+function hasSuffucientConstructsToBuild(construct) {
+    switch (construct) {
+        case Constructions.Road:
+            if (this.constructions[Constructions.Road] < 15)
+                return true;
+            break;
+        case Constructions.Settlement:
+            if (this.constructions[Constructions.Settlement] < 5)
+                return true;
+            break;
+        case Constructions.City:
+            if (this.constructions[Constructions.City] < 4 && this.constructions[Constructions.Settlement] > 0)
+                return true;
+            break;
+        case Constructions.DevelopmentCard:
+            return true;
+            break;
+        default:
+            return false;
+            break;
+    }
+    return false;
+}
+function canBuildRoadLegally(board, row, col, edge) {
+    if (edge < 0 || edge > 5)
+        return false;
+    if (row < 0 || row > ROWS || col < 0 || col > COLS)
+        return false;
+    // edge must be empty - no other roads
+    if (board[row][col].edges[edge] >= 0)
+        return false;
+    var adjHex = getHexAdjcentToEdge(row, col, edge);
+    // both hexes cannot be water
+    if (board[row][col].label = Resource.Water) {
+        if (board[adjHex[0]][adjHex[1]].label = Resource.Water) {
+            return false;
+        }
+    }
+    // player owns adjacent road in current hex or adjacent road in adjacent hex
+    if (board[row][col].edges[((edge + 1) % 6 + 6) % 6] = this.id || )
+        ;
+    board[row][col].edges[((edge - 1) % 6 + 6) % 6] = this.id || ;
+    board[adjHex[0]][adjHex[1]].edges[((edge + 3 + 1) % 6 + 6) % 6] = this.id || ;
+    board[adjHex[0]][adjHex[1]].edges[((edge + 3 - 1) % 6 + 6) % 6] = this.id;
+    {
+        // check if other player's settlement/city is inbetween existing road and proposed road
+        // cannot build through player's settlement/city, even with connecting road
+        // building CC on same hex
+        if (board[row][col].edges[((edge + 1) % 6 + 6) % 6] = this.id && )
+            ;
+        ((board[row][col].vertices[edge] == Constructions.Settlement || ));
+        board[row][col].vertices[edge] == Constructions.City;
+         && ;
+        board[row][col].vertexOwner[edge] != this.id;
+        {
+            return false;
+        }
+        // building CW on same hex
+        if (board[row][col].edges[((edge - 1) % 6 + 6) % 6] = this.id && )
+            ;
+        ((board[row][col].vertices[((edge - 1) % 6 + 6) % 6] == Constructions.Settlement || ));
+        board[row][col].vertices[((edge - 1) % 6 + 6) % 6] == Constructions.City;
+         && ;
+        board[row][col].vertexOwner[((edge - 1) % 6 + 6) % 6] != this.id;
+        {
+            return false;
+        }
+        // building CC on adj. hex
+        if (board[adjHex[0]][adjHex[1]].edges[((edge + 3 - 1) % 6 + 6) % 6] = this.id && )
+            ;
+        ((board[row][col].vertices[edge] == Constructions.Settlement || ));
+        board[row][col].vertices[edge] == Constructions.City;
+         && ;
+        board[row][col].vertexOwner[edge] != this.id;
+        {
+            return false;
+        }
+        // building CW on adj. hex
+        if (board[adjHex[0]][adjHex[1]].edges[((edge + 3 + 1) % 6 + 6) % 6] = this.id && )
+            ;
+        ((board[row][col].vertices[((edge - 1) % 6 + 6) % 6] == Constructions.Settlement || ));
+        board[row][col].vertices[((edge - 1) % 6 + 6) % 6] == Constructions.City;
+         && ;
+        board[row][col].vertexOwner[((edge - 1) % 6 + 6) % 6] != this.id;
+        {
+            return false;
+        }
+        return true;
+    }
+    return false;
+}
+function canBuildSettlementLegally(board, row, col, vertex) {
+    if (edge < 0 || edge > 5)
+        return false;
+    if (row < 0 || row > ROWS || col < 0 || col > COLS)
+        return false;
+    // proposed vertex must be empty - no other settlement/city
+    if (board[row][col].vertices[vertex] != -1)
+        return false;
+    // needs adjacent road to build on
+    if (!hasAdjacentRoad(board, row, col, vertex))
+        return false;
+    // new settlement has to be 2+ vertices away from another settlement/city
+    if (hasNearbyConstruct(board, row, col, vertex))
+        return false;
+    return true;
+}
+function canUpgradeSettlement(board, row, col, vertex) {
+    if (edge < 0 || edge > 5)
+        return false;
+    if (row < 0 || row > ROWS || col < 0 || col > COLS)
+        return false;
+    // proposed vertex must be empty - no other settlement/city
+    if (board[row][col].vertices[vertex] == Constructions.Settlement && )
+        ;
+    board[row][col].VertexOwner[vertex] == this.id;
+    return true;
+    return false;
+}
+function hasAdjacentRoad(board, row, col, vertex) {
+    if (board[row][col].edges[vertex] = this.id)
+        return true;
+    if (board[row][col].edges[(vertex + 1) % 6] = this.id)
+        return true;
+    var _a = getHexesAdjacentToVertex(row, col, vertex), hex1 = _a[0], hex2 = _a[1];
+    if (board[hex1[0]][hex1[1]].edges[hex1[3]] == this.id)
+        return true;
+    if (board[hex1[0]][hex1[1]].edges[(hex1[3] + 1) % 6] == this.id)
+        return true;
+    if (board[hex2[0]][hex2[1]].edges[hex2[3]] == this.id)
+        return true;
+    if (board[hex2[0]][hex2[1]].edges[(hex2[3] + 1) % 6] == this.id)
+        return true;
+    return false;
+}
+// *****************
+// Helper functions for player interface
+// *****************
+function hasNearbyConstruct(board, row, col, vertex) {
+    if (board[row][col].vertices[vertex] == Construction.Settlement || )
+        ;
+    board[row][col].vertices[vertex] == Construction.City;
+    return true;
+    if (board[row][col].vertices[(vertex + 1) % 6] == Construction.Settlement || )
+        ;
+    board[row][col].vertices[(vertex + 1) % 6] == Construction.City || ;
+    board[row][col].vertices[((vertex - 1) % 6 + 6) % 6] == Construction.Settlement || ;
+    board[row][col].vertices[((vertex - 1) % 6 + 6) % 6] == Construction.City;
+    return true;
+    var _a = getHexesAdjacentToVertex(row, col, vertex), hex1 = _a[0], hex2 = _a[1];
+    if (board[hex1[0]][hex1[1]].vertex[(hex1[3] + 1) % 6] == Construction.Settlement || )
+        ;
+    board[hex1[0]][hex1[1]].vertex[(hex1[3] + 1) % 6] == Construction.City || ;
+    board[hex1[0]][hex1[1]].vertices[((hex1[3] - 1) % 6 + 6) % 6] == Construction.Settlement || ;
+    board[hex1[0]][hex1[1]].vertices[((hex1[3] - 1) % 6 + 6) % 6] == Construction.City;
+    return true;
+    if (board[hex2[0]][hex2[1]].vertex[(hex2[3] + 1) % 6] == Construction.Settlement || )
+        ;
+    board[hex2[0]][hex2[1]].vertex[(hex2[3] + 1) % 6] == Construction.City || ;
+    board[hex2[0]][hex2[1]].vertices[((hex2[3] - 1) % 6 + 6) % 6] == Construction.Settlement || ;
+    board[hex2[0]][hex2[1]].vertices[((hex2[3] - 1) % 6 + 6) % 6] == Construction.City;
+    return true;
+    return false;
+}
+// return [row, col] pair that shares edge with original edge
+function getHexAdjcentToEdge(row, col, edge) {
+    // TODO: add check for new row/col out of bounds
+    if (edge == 0)
+        return [row, col + 1];
+    if (edge == 1 && row % 2 == 0)
+        return [row - 1, col + 1];
+    if (edge == 1 && row % 2 == 1)
+        return [row - 1, col];
+    if (edge == 2 && row % 2 == 0)
+        return [row - 1, col];
+    if (edge == 2 && row % 2 == 1)
+        return [row - 1, col - 1];
+    if (edge == 3)
+        return [row, col - 1];
+    if (edge == 4 && row % 2 == 0)
+        return [row + 1, col];
+    if (edge == 4 && row % 2 == 1)
+        return [row + 1, col - 1];
+    if (edge == 5 && row % 2 == 0)
+        return [row + 1, col + 1];
+    if (edge == 5 && row % 2 == 1)
+        return [row + 1, col];
+    // default - shouldn't happen though
+    return [-1, -1];
+}
+// will return 2 hexes in [row, col, vertex] that share the original vertex
+function getHexesAdjacentToVertex(row, col, vertex) {
+    // TODO: add check for new row/col hexes out of bounds
+    if (vertex == 0 && row % 2 == 0)
+        return [[row, col + 1, 2], [row - 1, col + 1, 4]];
+    if (vertex == 0 && row % 2 == 1)
+        return [[row, col + 1, 2], [row - 1, col, 4]];
+    if (vertex == 1 && row % 2 == 0)
+        return [[row - 1, col, 5], [row - 1, col + 1, 3]];
+    if (vertex == 1 && row % 2 == 1)
+        return [[row - 1, col - 1, 5], [row - 1, col, 3]];
+    if (vertex == 2 && row % 2 == 0)
+        return [[row, col - 1, 0], [row - 1, col, 4]];
+    if (vertex == 2 && row % 2 == 1)
+        return [[row, col - 1, 0], [row - 1, col - 1, 4]];
+    if (vertex == 3 && row % 2 == 0)
+        return [[row, col - 1, 5], [row + 1, col, 1]];
+    if (vertex == 3 && row % 2 == 1)
+        return [[row, col - 1, 5], [row + 1, col - 1, 1]];
+    if (vertex == 4 && row % 2 == 0)
+        return [[row + 1, col, 0], [row + 1, col + 1, 2]];
+    if (vertex == 4 && row % 2 == 1)
+        return [[row + 1, col - 1, 0], [row + 1, col, 2]];
+    if (vertex == 5 && row % 2 == 0)
+        return [[row + 1, col + 1, 1], [row, col + 1, 3]];
+    if (vertex == 5 && row % 2 == 1)
+        return [[row + 1, col, 1], [row, col + 1, 3]];
+    // default - shouldn't happen though
+    return [[-1, -1, -1], [-1, -1, -1]];
+}
 //# sourceMappingURL=common.js.map
