eaed410b05752a0e21571093fd7d71c32d5d7bbf
ted-wong <wong.ted.t@gmail.com>
2016-02-19 12:04:25 -0500

    Prepare to merge

---
+++ ts_output_readonly_do_NOT_change_manually/gameLogic.js
@@ -0,0 +1,199 @@
+var gameLogic;
+(function (gameLogic) {
+    gameLogic.ROWS = 7;
+    gameLogic.COLS = 7;
+    gameLogic.NUM_PLAYERS = 4;
+    function getInitialBoard() {
+        var board = [];
+        //TODO: Shuffle & terrains
+        for (var i = 0; i < gameLogic.ROWS; i++) {
+            board[i] = [];
+            for (var j = 0; j < gameLogic.COLS; j++) {
+                var edges = [-1, -1, -1, -1, -1, -1];
+                var vertices = [-1, -1, -1, -1, -1, -1];
+                var hex = {
+                    label: Resource.Dust,
+                    edges: edges,
+                    vertices: vertices,
+                    rollNum: -1,
+                    tradingRatio: 4,
+                    hasRobber: false
+                };
+                board[i][j] = hex;
+            }
+        }
+        return board;
+    }
+    function getInitialArray(size) {
+        var ret = [];
+        for (var i = 0; i < size; i++) {
+            ret[i] = 0;
+        }
+        return ret;
+    }
+    function getInitialPlayers() {
+        var players = [];
+        for (var i = 0; i < gameLogic.NUM_PLAYERS; i++) {
+            players[i] = {
+                id: i,
+                points: 0,
+                resources: getInitialArray(Resource.SIZE),
+                devCards: getInitialArray(DevCard.SIZE),
+                knightsPlayed: 0,
+                longestRoad: 0,
+                constructions: getInitialArray(Construction.SIZE)
+            };
+        }
+        return players;
+    }
+    function getInitialBank() {
+        var bank = {
+            resources: getInitialArray(Resource.SIZE),
+            devCards: getInitialArray(DevCard.SIZE)
+        };
+        //Assign total size of resources/devCards in bank according to rules
+        for (var i = 0; i < Resource.SIZE; i++) {
+            bank.resources[i] = 19;
+        }
+        for (var i = 0; i < DevCard.SIZE; i++) {
+            switch (i) {
+                case DevCard.Knight:
+                    bank.devCards[i] = 14;
+                case DevCard.Monopoly:
+                    bank.devCards[i] = 2;
+                case DevCard.RoadBuilding:
+                    bank.devCards[i] = 2;
+                case DevCard.YearOfPlenty:
+                    bank.devCards[i] = 2;
+                case DevCard.VictoryPoint:
+                    bank.devCards[i] = 5;
+                default:
+                    break;
+            }
+        }
+        return bank;
+    }
+    function getInitialAwards() {
+        return {
+            longestRoad: {
+                player: -1,
+                length: 4
+            },
+            largestArmy: {
+                player: -1,
+                num: 2
+            }
+        };
+    }
+    function getInitialRobber(board) {
+        var row = -1;
+        var col = -1;
+        for (var i = 0; i < gameLogic.ROWS; i++) {
+            for (var j = 0; j < gameLogic.COLS; j++) {
+                if (board[i][j].hasRobber) {
+                    row = i;
+                    col = j;
+                    break;
+                }
+            }
+        }
+        return { row: row, col: col };
+    }
+    function getInitialState() {
+        var board = getInitialBoard();
+        var robber = getInitialRobber(board);
+        return {
+            board: board,
+            dices: [1, 1],
+            players: getInitialPlayers(),
+            bank: getInitialBank(),
+            awards: getInitialAwards(),
+            robber: robber,
+            diceRolled: false,
+            devCardsPlayed: false,
+            delta: null,
+            moveType: MoveType.INIT,
+            eventIdx: -1
+        };
+    }
+    gameLogic.getInitialState = getInitialState;
+    /**
+     * Validation logics
+     */
+    function rollDice(prevState, nextState, idx) {
+        if (prevState.diceRolled) {
+            throw new Error('Dices already rolled');
+        }
+    }
+    function checkRobberEvent(prevState, nextState, idx) {
+        var prevSum = 0;
+        var nextSum = 0;
+        for (var i = 0; i < Resource.SIZE; i++) {
+            prevSum += prevState.players[idx].resources[i];
+            nextSum += nextState.players[idx].resources[i];
+        }
+        if (prevSum > 7 && nextSum > prevSum / 2) {
+            throw new Error('Need to toss half of resource cards');
+        }
+    }
+    function checkRobberMove(prevState, nextState, idx) {
+        if (angular.equals(prevState.robber, nextState.robber)) {
+            throw new Error('Need to move robber');
+        }
+    }
+    function checkResources(resources) {
+        for (var i = 0; i < Resource.SIZE; i++) {
+            if (resources[i] < 0) {
+                throw new Error('Insufficient resources');
+            }
+        }
+    }
+    function checkTradeResourceWithBank(prevState, nextState, idx) {
+        if (!prevState.diceRolled) {
+            throw new Error('Need to roll dices first');
+        }
+        var selling = { item: Resource.Dust, num: 0 };
+        var buying = { item: Resource.Dust, num: 0 };
+        checkResources(nextState.players[idx].resources);
+        for (var i = 0; i < Resource.SIZE; i++) {
+            if (nextState.players[idx].resources[i] < prevState.players[idx].resources[i]) {
+                selling = {
+                    item: i,
+                    num: prevState.players[idx].resources[i] - nextState.players[idx].resources[i]
+                };
+            }
+            if (nextState.players[idx].resources[i] > prevState.players[idx].resources[i]) {
+                buying = {
+                    item: i,
+                    num: nextState.players[idx].resources[i] - prevState.players[idx].resources[i]
+                };
+            }
+        }
+        if (selling.item === buying.item) {
+            throw new Error('Cannot trade the same resources');
+        }
+        //TODO: Need to integrate with harbors
+        if (buying.num * 4 !== selling.num) {
+            throw new Error('Wrong trading ratio');
+        }
+    }
+    /**
+    * XXX: Assuming UI will disable this feature when bank has no dev cards
+    */
+    function checkBuildDevCards(prevState, nextState, idx) {
+        if (!prevState.diceRolled) {
+            throw new Error('Need to roll dices first');
+        }
+        checkResources(nextState.players[idx].resources);
+    }
+    function checkPlayDevCard(prevState, nextState, idx) {
+        if (prevState.devCardsPlayed) {
+            throw new Error('Already played development cards');
+        }
+        //TODO: Check when playing year of plenty
+    }
+    function checkMoveOk(stateTransition) {
+    }
+    gameLogic.checkMoveOk = checkMoveOk;
+})(gameLogic || (gameLogic = {}));
+//# sourceMappingURL=gameLogic.js.map
