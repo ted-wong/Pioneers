7ee67d78d420aa5966bbb96ecb56319e0a15301d
GabrielWen <gabriel.weng@gmail.com>
2016-02-20 17:06:24 -0500

    unit test config

---
+++ src/gameLogic_test.ts
@@ -0,0 +1,20 @@
+describe('Initial state tests', function() {
+  it('First Test', function() {
+  });
+
+  it('Test3', function() {
+  });
+});
+
+describe('Robber related validation', function() {
+  it('TEST2', function() {
+  });
+});
+
+describe('Playing dev cards validation', function() {
+
+});
+
+describe('Trading with bank validation', function() {
+
+});
